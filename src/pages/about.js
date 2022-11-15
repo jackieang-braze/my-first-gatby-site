6b26a0052a2bbd0ba43efb1cd2bfdf7b927ed981
jackieang-braze <116116326+jackieang-braze@users.noreply.github.com>
2022-11-15 11:33:43 -0800

    Nav bar

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,24 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    <Layout pageTitle="About Me">
+      <p>Hi there! My name's Jackie :D</p>
+    </Layout>
   )
 }
+
+export const Head = () => (
+    <>
+        <title>About Me</title>
+        <meta name="description" content="Your description"/>
+    </>
+    )
+
 
 // Step 3: Export your component
 export default AboutPage
