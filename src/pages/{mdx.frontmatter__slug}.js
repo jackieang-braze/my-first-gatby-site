36d76e30e411841f588a40b747d527857fb9159f
jackieang-braze <116116326+jackieang-braze@users.noreply.github.com>
2022-11-18 08:58:57 -0800

    file name error

---
+++ src/pages/{mdx.frontmatter__slug}.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const BlogPost = () => {
+  return (
+    <Layout pageTitle="Blog Posts">
+      <p>My blog post contents will go here (eventually).</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="Blog Posts" />
+
+export default BlogPost
